0b98d8837b33782345bebd8752b74fc9138d46b9
Jhonattan Solarte Martinez <jhonattan.solartemartinez@betssongroup.com>
2024-06-03 17:15:09 +0200

    initial project, menu topics

---
+++ bootcamp/bootcamp/ContentView.swift
@@ -0,0 +1,51 @@
+import SwiftUI
+
+enum TopicView: Int, Identifiable, CaseIterable {
+    case customBinding = 1
+    case otherTopic = 2
+    
+    var id: Int { rawValue }
+    
+    var title: String {
+        switch self {
+        case .customBinding: return "Custom Binding"
+        default: return "Sin titulo"
+        }
+    }
+    
+    var Description: String {
+        switch self {
+        case .customBinding: return "Creating a custom Binding"
+        default: return "Not available"
+        }
+    }
+    
+    @ViewBuilder
+    var view: some View {
+        switch self {
+        case .customBinding: Text("one")
+        default: Text("Other")
+        }
+    }
+}
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            List(TopicView.allCases) { topic in
+                NavigationLink(destination: topic.view){
+                    VStack(alignment: .leading) {
+                        Text(topic.title)
+                            .fontWeight(.regular)
+                        Text(topic.Description)
+                            .font(.footnote)
+                    }
+                }
+            }
+        }
+        .navigationTitle("Topics")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bootcamp/bootcamp/bootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  bootcampApp.swift
+//  bootcamp
+//
+//  Created by Jhonattan Solarte Martinez on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct bootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
